2aaf40d2c709aa1cafe950ee2f3fe9271487568c
Bogdawaa <bogdan6246@yandex.ru>
2025-08-21 20:41:26 +0300

    initial commit

---
+++ LoginProfileTestApp/Application/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  LoginProfileTestApp
+//
+//  Created by Bogdan Fartdinov on 21.08.2025.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = LoginViewController()
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
+
