a33c972c3830776b291afdcd9ad9f66503f3ff75
Bogdawaa <bogdan6246@yandex.ru>
2025-08-21 20:04:25 +0300

    Initial Commit

---
+++ LoginProfileTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginProfileTestApp
+//
+//  Created by Bogdan Fartdinov on 21.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
